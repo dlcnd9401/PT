32574367ac47ea0e9396567d49e45c210917a53a
GD <GD@DESKTOP-TL3UJ6Q>
2018-01-31 18:03:55 +0900

    완료

---
+++ js/app.js
@@ -0,0 +1,109 @@
+var gApp = angular.module('gApp', []);
+    gApp.run(function($rootScope){
+    	$rootScope.address = "서울시 금천구 가산디지털2로 115 대륭테크노타운3차";
+    	$rootScope.dns = "GooDee";
+    	$rootScope.title = "Portfolio";
+    	$rootScope.name = "이충민";
+    });
+	gApp.controller('gCtrl', function($scope) {
+		$scope.htmlCheck = false;
+		$scope.bodyCheck = false;
+		$scope.btCheck = false;
+		$scope.projectFlag = false;
+		$scope.projectUrl = "";
+		$scope.btnActive = 1;
+		
+		$scope.dropEvent = function() {
+			$scope.htmlCheck = !$scope.htmlCheck;
+			$scope.bodyCheck = !$scope.bodyCheck;
+			$scope.btCheck   = !$scope.btCheck;
+		};
+		
+		$scope.projectEvent = function(rows) {
+			$scope.row = rows;
+			if($scope.projectUrl == rows.url) {
+				$scope.projectUrl = "";
+				$scope.projectFlag = false;
+			} else {
+				$scope.projectUrl = rows.url;
+				$scope.projectFlag = true;
+			}
+		}
+		
+		$scope.iFrameLink = function(){
+			if($scope.iframeView){
+				location.href = $scope.iframeView;
+			}
+		}
+		
+		$scope.btnList = [
+			{filter: "*",      name: "All",      active: true },
+			{filter: ".bgOn",  name: "Personal", active: false},
+			{filter: ".bgOff", name: "Team",     active: false}
+		];
+		
+		$scope.dataSource = [
+			{
+			 path: "portfolio/",
+			 url : "team/team.pdf", 
+			 title: "Team",
+			 name: "Impression",
+			 img: "team/TeamImpression.png",
+			 type : true, 
+			 contents: "  교육 기관에서 6개월간 교육을 받은 뒤 제작한 사이트입니다. 처음으로 혼자 구현한 사이트이고, 평소 관심을 가지고 있던 분야라 애착이 많이 가는 사이트 입니다. 기획부터 프로그램 개발, 테스트까지 전반적인 과정을 혼자 하였기에 부족한 점이 많습니다. 후에, 디자인적 요소와 반응형 웹을 추가하여, 프로젝트의 완성도를 채울 것 입니다."
+			},{
+			 path: "portfolio/",
+			 url : "personal/personal.pdf", 
+			 title: "Personal",
+			 name: "Impression",
+			 img: "personal/PersonalImpression.png",
+			 type : false,
+			 contents: "  처음으로 제작한 벤치마킹 사이트 입니다. 저를 포함한 모든 팀원들이 처음 진행하는 것이라, 많은 어려움이 있었지만, 서로 책임감 있는 모습을 보여, 완성할 수 있어서 뿌듯했습니다. 팀장으로 프로젝트를 진행함에 있어 서로 간의 의사소통 부재, 자신의 실력, 무리한 계획 등 미흡한 부분을 알아 갈 수 있어서 저에게 좋은 경험이 되었습니다."
+			},{
+			 path: "media/",
+			 url : "personal.mp4", 
+			 title: "Personal",
+			 name: "Media",
+			 img: "personal/PersonalMedia.png",
+			 type : false, 
+			 contents: ""
+			}
+		];
+		
+		$scope.btnEvnet = function(index){
+			$scope.projectUrl = "";
+			$scope.projectFlag = false;
+			
+			for(var i = 0; i < $scope.btnList.length; i++){
+				$scope.btnList[i].active = false;
+			}
+			$scope.btnList[index].active = true;
+			$scope.grid.isotope({ filter: $scope.btnList[index].filter });
+		}
+		
+		setTimeout(function(){
+			$scope.grid = $('#portfolioGroup').isotope();
+		}, 200);
+	});
+	gApp.directive('resize', function ($window) {
+	    return function (scope, element) {
+	        var w = angular.element($window);
+	        scope.getWindowDimensions = function () {
+	            return {
+	                'h': w.height(),
+	                'w': w.width()
+	            };
+	        };
+	        scope.$watch(scope.getWindowDimensions, function (newValue, oldValue) {
+	            if(newValue.w >= 768){
+					scope.htmlCheck = false;
+					scope.bodyCheck = false;
+					scope.btCheck = false;
+				}
+	        }, true);
+
+	        w.bind('resize', function () {
+	            scope.$apply();
+	        });
+	    }
+	});
